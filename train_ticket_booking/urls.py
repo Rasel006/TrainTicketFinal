2dd5cbe913e9ee783194e2e128819a48b3320478
Rasel006 <rizurasel.cse@gmail.com>
2024-04-28 10:37:58 +0600

    url update

--- train_ticket_booking/urls.py
+++ train_ticket_booking/urls.py
@@ -20,11 +20,12 @@ from django.urls import path,include
 from core.views import HomeView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+      path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path('', HomeView.as_view(), name='home'),
     path('accounts/', include('account.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('train/', include('train.urls')),
     path('<slug:station_slug>', HomeView.as_view(),name='station_wise'),
-    path('transaction', include('transactions.urls')),
+    path('transaction', include('transaction.urls')),
 ]
