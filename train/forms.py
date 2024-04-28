508f71fe0c0b468d3f342e2447f3e97a4b699e26
Rasel006 <rizurasel.cse@gmail.com>
2024-04-28 11:07:52 +0600

    forms update

--- train/forms.py
+++ train/forms.py
@@ -1,48 +1,12 @@
 from django import forms
 from .models import Train_list
-from .models import UserReviews,TrainPurchase
 
-STAR_CHOICES = [
-    ('⭐', '⭐'),
-    ('⭐⭐', '⭐⭐'),
-    ('⭐⭐⭐', '⭐⭐⭐'),
-    ('⭐⭐⭐⭐', '⭐⭐⭐⭐'),
-    ('⭐⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'),
-]  
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model = UserReviews
-        fields = ['rating', 'body']
-        widgets = {
-            'rating': forms.Select(choices=STAR_CHOICES),
-            'body': forms.Textarea(attrs={'rows': 4}),
-        }
-        
-    def __init__(self, *args, **kwargs):
-        self.train = kwargs.pop('train', None)
-        self.user = kwargs.pop('user', None)
-        super().__init__(*args, **kwargs)
-
-    def clean(self):
-        cleaned_data = super().clean()
-
-        user_purchased_or_borrowed = TrainPurchase.objects.filter(user=self.user, train=self.train).exists()
-
-        if not user_purchased_or_borrowed:
-            raise forms.ValidationError("You must purchase the Ticket to leave a review.")
-
-        return cleaned_data
 class AddTrainForm(forms.ModelForm):
     class Meta:
         model = Train_list
-        fields = ['name', 'time', 'station', 'destination','seat_number','price']
+        fields = ['name', 'time', 'station', 'destination', 'price', 'seat_formation']
 
 class TrainInfoUpdateForm(forms.ModelForm):
     class Meta:
         model= Train_list
-        fields = ['name', 'time', 'destination','seat_number','price']
-
-class ReviewUpdateForm(forms.ModelForm):
-    class Meta:
-        model = UserReviews
-        fields = ['rating', 'body']
+        fields = ['name', 'time', 'station','destination']
